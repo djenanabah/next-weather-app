020f415e81faeb76b80d9ec8e18b8fb37d349a5b
djenaba bah <djenaba.bah@epitech.eu>
2022-08-06 00:04:36 +0200

    extract card item into component

--- pages/app.tsx
+++ pages/app.tsx
@@ -1,8 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import CardItem from '../components/CardItem'
 import styles from '../styles/App.module.scss'
-import { WiDaySunny, WiDayCloudyHigh } from "react-icons/wi";
 
 const App: NextPage = () => {
   return (
@@ -14,41 +13,7 @@ const App: NextPage = () => {
       </Head>
 
       <div className={styles.cardList}>
-        <div className={styles.cardItem}>
-          <div className={styles.cardItemTodayInfo}>
-            <h2>Paris</h2>
-            <div className={styles.cardItemIcon}>
-              <WiDaySunny className={`${styles.cardItemSvg} ${styles.big}`}/>
-              <p>Météo clair</p>
-            </div>
-            <div className={styles.cardItemCurrentWeather}>
-              <p>30°C</p>
-              <p>18:10</p>
-            </div>
-          </div>
-          <div className={styles.cardItemWeekInfo}>
-            <div className={styles.cardItemWeekday}>
-              <h3>Lundi</h3>
-              <WiDaySunny className={styles.cardItemSvg}/>
-              <p><span>20</span> - <span>30</span></p>
-            </div>
-            <div className={styles.cardItemWeekday}>
-              <h3>Mardi</h3>
-              <WiDaySunny className={styles.cardItemSvg}/>
-              <p><span>20</span> - <span>30</span></p>
-            </div>
-            <div className={styles.cardItemWeekday}>
-              <h3>Mercredi</h3>
-              <WiDaySunny className={styles.cardItemSvg}/>
-              <p><span>20</span> - <span>30</span></p>
-            </div>
-            <div className={styles.cardItemWeekday}>
-              <h3>Jeudi</h3>
-              <WiDaySunny className={styles.cardItemSvg}/>
-              <p><span>20</span> - <span>30</span></p>
-            </div>
-          </div>
-        </div>
+        <CardItem />
       </div>
     </div>
   )
